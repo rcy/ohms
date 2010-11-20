924f7221cdab989522c3a45e0cde8e3b44f16fbf
Ryan Yeske <rcyeske@gmail.com>
2010-11-19 18:57:58 -0800

    display data from couch in a treeview

--- public/datasource.js
+++ public/datasource.js
@@ -1,9 +1,12 @@
 var templates = {};
 var root;
 
-YUI().use('datasource', function(Y) {
-  var templateDS = new Y.DataSource.IO({source:"/api/template"});
+YUI().use('datasource', 'gallery-treeview', function(Y) {
+  //Associate the YAHOO variable with and instance of Dav Glass's
+  //Port utility:
+  var YAHOO = Y.Port();
 
+  var templateDS = new Y.DataSource.IO({source:"/api/template"});
   //Normalize the data sent to myCallback
   templateDS.plug({fn: Y.Plugin.DataSourceJSONSchema, cfg: {
     schema: {
@@ -12,13 +15,25 @@ YUI().use('datasource', function(Y) {
     }
   }});
 
+  // setup the treeview
+  var tree = new YAHOO.widget.TreeView("templateTree");
+  var rootNode = tree.getRoot();
+  var nodes = {};
+
+  // request templates in database
   templateDS.sendRequest({
     request: "",
     callback: { 
       success: function(e){
         console.log(e.response);
         // for each template doc, add it to the tree
-        
+        Y.Array.each(e.response.results, function(elt) {
+          var id = elt.id;
+          var label = elt.value.name;
+          var parent_id = elt.value.parent;
+          nodes[id] = new YAHOO.widget.TextNode(label, nodes[parent_id] || rootNode);
+        });
+        tree.render();
       },
       failure: function(e){
         console.log(e.error.message);
